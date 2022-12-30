5d792860a3bf67c15ab6e3c9b1dfa13786e7451f
rufous86 <iliukhinandrei@gmail.com>
2022-12-30 15:04:15 +0300

    c

--- parcing.py
+++ parcing.py
@@ -1,11 +1,12 @@
 import requests
 import pandas as pd
+import numpy as np
 
 
 def get_vacancy(skills, pages=100):
     res = []
-    for skill in skills:
-        print(f'collecting <{skill}>')
+    for indx, skill in enumerate(skills):
+        print(f'collecting <{skill}> ({indx+1} of {len(skills)})')
         for page in range(pages):
             params = {
                 'text': f'{skill}',
@@ -19,11 +20,46 @@ def get_vacancy(skills, pages=100):
     return res
 
 
-skill_list = ['machine AND learning', 'data AND science', 'sql', 'NLP',
+skill_list = ['machine AND learning', 'df AND science', 'sql', 'NLP',
               'spark', 'hadoop', 'pandas', 'dask', 'deep AND learning', 'pytorch',
               'tensorflow', 'keras', 'ai AND developer', 'computer AND vision',
               'нейронные AND сети', 'big AND data']
 
-data = pd.DataFrame(get_vacancy(skill_list))
+df = pd.DataFrame(get_vacancy(skill_list))
 
-data.to_csv('data.csv', index=False)
+df = df[['name', 'salary', 'schedule', 'accept_temporary', 'area', 'published_at']]
+
+
+df["city"] = (df["area"]
+                     .apply(lambda x: x.get("name")
+                                      if isinstance(x, dict)
+                                      else np.nan))
+df["schedule"] = (df["schedule"]
+                     .apply(lambda x: x.get("id")
+                                      if isinstance(x, dict)
+                                      else np.nan))
+df["salary_from"] = (df["salary"]
+                     .apply(lambda x: x.get("from")
+                                      if isinstance(x, dict)
+                                      else np.nan))
+df["salary_to"] = (df["salary"]
+                     .apply(lambda x: x.get("to")
+                                      if isinstance(x, dict)
+                                      else np.nan))
+df["salary_currency"] = (df["salary"]
+                     .apply(lambda x: x.get("currency")
+                                      if isinstance(x, dict)
+                                      else np.nan))
+
+df = df[df['salary_currency'] == 'RUR']
+df = df.drop(['salary', 'area'], axis=1)
+df = df.drop_duplicates()
+df['salary_from'] = df['salary_from'].fillna(df['salary_to'])
+df['salary_to'] = df['salary_to'].fillna(df['salary_from'])
+df = df.dropna(subset=['salary_from', 'salary_to'])
+df['salary_mean'] = round((df['salary_from'] + df['salary_to']) / 2)
+df = df.drop(['salary_currency', 'salary_from', 'salary_to'], axis=1)
+
+print(df.info())
+
+df.to_csv('data.csv', index=False)
